b9fd45ba9b9d8b40043906c9685ac7e6b5bb99f8
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2021-12-01 06:58:06 +0200

    add template/task.js

---
+++ js/template/task.js
@@ -0,0 +1,13 @@
+// const rawData = require('fs').readFileSync('testinput', 'UTF-8').split('\n');
+const rawData = require('fs').readFileSync('input', 'UTF-8').split('\n');
+rawData.pop();
+
+console.time("Part 1");
+(() => {
+})();
+console.timeEnd("Part 1");
+
+console.time("Part 2");
+(() => {
+})();
+console.timeEnd("Part 2");
