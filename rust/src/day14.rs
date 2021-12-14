aa3fb1b215c5b3be77e1255d553407b9d071ef67
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2021-12-14 13:49:11 +0200

    day14.rs: calculate frequencies once per pair

--- rust/src/day14.rs
+++ rust/src/day14.rs
@@ -4,6 +4,7 @@ type Pair = (u8, u8);
 pub struct Input {
     template: HashMap<Pair, usize>,
     rules: HashMap<Pair, u8>,
+    last: u8,
 }
 
 impl FromStr for Input {
@@ -11,16 +12,15 @@ impl FromStr for Input {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut lines = s.lines();
-        let template = lines
-            .next()
-            .unwrap()
-            .as_bytes()
+        let template_string = lines.next().unwrap().as_bytes();
+        let template = template_string
             .windows(2)
             .map(|slice| (slice[0], slice[1]))
             .fold(HashMap::new(), |mut acc, v| {
                 *acc.entry(v).or_insert(0) += 1;
                 acc
             });
+        let last = *template_string.last().unwrap();
 
         lines.next();
 
@@ -33,7 +33,11 @@ impl FromStr for Input {
             },
         );
 
-        Ok(Self { template, rules })
+        Ok(Self {
+            template,
+            rules,
+            last,
+        })
     }
 }
 
@@ -60,21 +64,21 @@ impl Input {
         Self {
             template,
             rules: self.rules,
+            last: self.last,
         }
     }
 
     fn answer(self) -> usize {
-        let freqs = self
-            .template
-            .into_iter()
-            .fold(HashMap::new(), |mut acc, (k, v)| {
+        let freqs = self.template.into_iter().fold(
+            [(self.last, 1)].into(),
+            |mut acc: HashMap<u8, usize>, (k, v)| {
                 *acc.entry(k.0).or_insert(0) += v;
-                *acc.entry(k.1).or_insert(0) += v;
                 acc
-            });
+            },
+        );
         let min = freqs.values().min().unwrap();
         let max = freqs.values().max().unwrap();
-        (1 + max - min) / 2
+        max - min
     }
 }
 
